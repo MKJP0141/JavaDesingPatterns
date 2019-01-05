101f6569170ed2b456b8923e48e40d924b2838a8
MKJP0141 <easyqm-dumpit@yahoo.co.jp>
2019-01-05 20:58:54 +0900

    Template Methodの実用的なサンプルコード

--- TemplateMethodReal/Program.cs
+++ TemplateMethodReal/Program.cs
@@ -6,7 +6,21 @@ namespace TemplateMethodReal
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Document[] documents = new Document[2];
+
+            documents[0] = new Resume();
+            documents[1] = new Report();
+
+            foreach (Document document in documents)
+            {
+                Console.WriteLine("\n" + document.GetType().Name + "--");
+                foreach (Page page in document.Pages)
+                {
+                    Console.WriteLine(" " + page.GetType().Name);
+                }
+            }
+
+            Console.ReadKey();
         }
     }
 }
