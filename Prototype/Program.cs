e910727664927b7041859ab4882f3e5763412886
MKJP0141 <easyqm-dumpit@yahoo.co.jp>
2019-01-05 23:52:35 +0900

    Prototypeパターンの実用的なサンプルコードを実装

--- Prototype/Program.cs
+++ Prototype/Program.cs
@@ -6,7 +6,21 @@ namespace Prototype
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ColorManager colorManager = new ColorManager();
+
+            colorManager["red"] = new Color(255, 0, 0);
+            colorManager["green"] = new Color(0, 255, 0);
+            colorManager["blue"] = new Color(0, 0, 255);
+
+            colorManager["angry"] = new Color(255, 54, 0);
+            colorManager["peace"] = new Color(128,211,128);
+            colorManager["flame"] = new Color(211, 34, 20);
+
+            Color color1 = colorManager["red"].Clone() as Color;
+            Color color2 = colorManager["peace"].Clone() as Color;
+            Color color3 = colorManager["flame"].Clone() as Color;
+
+            Console.ReadKey();
         }
     }
 }
