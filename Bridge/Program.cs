762171645b6268d12d3b18dcc391347c1759895e
MKJP0141 <easyqm-dumpit@yahoo.co.jp>
2019-02-03 23:25:44 +0900

    移植完了

--- Bridge/Program.cs
+++ Bridge/Program.cs
@@ -6,7 +6,13 @@ namespace Bridge
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Display d1 = new Display(new StringDisplayImpl("Hello, Japan."));
+            Display d2 = new Display(new StringDisplayImpl("Hello, World."));
+            CountDisplay d3 = new CountDisplay(new StringDisplayImpl("Hello, Universe."));
+            d1.DisplayText();
+            d2.DisplayText();
+            d3.DisplayText();
+            d3.MultiDisplay(5);
         }
     }
 }
