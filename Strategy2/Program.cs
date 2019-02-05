6fefdce85607ec9908857710b43327a970f0ec87
U-JP-MIC\m-kinoshita <m-kinoshita@m-kinoshita.jp-mic.local>
2019-02-05 14:07:23 +0900

    First Commit.

---
+++ Strategy2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Strategy2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] datas =
+            {
+                "Dumpty",
+                "Bowman",
+                "Carroll",
+                "Elfland",
+                "Alice",
+            };
+
+            SortAndPrint sap = new SortAndPrint(datas, new SelectionSorter());
+            sap.Execute();
+        }
+    }
+}
