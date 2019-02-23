9ec7b52e0861c7b3c91b8ba2d06a92f8a880334b
MKJP0141 <easyqm-dumpit@yahoo.co.jp>
2019-02-23 12:15:49 +0900

    First Commit for Visitor Real Example.

---
+++ Visitor/VisitorReal.Test/VisitorRealTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace VisitorReal.Test
+{
+    public class VisitorRealTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Visitor/VisitorReal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VisitorReal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
