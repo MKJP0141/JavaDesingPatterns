9a2f903a4fbc6d88f4a0582ec47d5abc413a2285
MKJP0141 <easyqm-dumpit@yahoo.co.jp>
2019-02-28 00:04:08 +0900

    Merge Commit.

---
+++ Visitor/MoqTest/Model/IEmployee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoqTest.Model
+{
+    public interface IEmployee
+    {
+        string GetFullName();
+
+        void ToCSV(string firstName, string lastName);
+    }
+}
