13ed84e07ca10fa5551b59538506b53275a45801
木下 宗則 <m-kinoshita@jp-mic.local>
2019-02-27 14:31:12 +0900

    MoqTestクラスを追加

---
+++ Visitor/MoqTest/Model/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoqTest.Model
+{
+    class Employee : IEmployee
+    {
+        private string _fistName;
+        private string _lastName;
+
+        public Employee(string firstName, string lastName)
+        {
+            _fistName = firstName;
+            _lastName = lastName;
+        }
+
+        public string GetFullName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ToCSV(string firstName, string lastName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Visitor/VisitorReal.Test/MoqTestTest/MoqTestClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Moq;
+using MoqTest.Model;
+using Xunit;
+
+namespace VisitorReal.Test.MoqTestTest
+{
+    public class MoqTestClass
+    {
+        [Fact]
+        public void GetFullNameTest()
+        {
+            var fullName = "山田 太郎";
+            var mock = new Mock<IEmployee>();
+            mock.Setup(x => x.GetFullName())
+                .Returns(() => fullName);
+
+            Assert.Equal(fullName, mock.Object.GetFullName());
+        }
+
+        [Fact]
+        public void ToCSVTest()
+        {
+            var mock = new Mock<IEmployee>();
+            var csv = string.Empty;
+            mock.Setup(x => x.ToCSV(It.IsAny<string>(), It.IsAny<string>()))
+                .Callback<string, string>((l, f) => csv = $"{l},{f}");
+                                
+
+            mock.Object.ToCSV("山田", "太郎");
+            Assert.Equal("山田,太郎", csv);
+            mock.Verify();
+        }
+    }
+}
