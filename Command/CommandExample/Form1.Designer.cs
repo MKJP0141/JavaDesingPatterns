cb85e8173a631fc868e833e1831611ec1042f020
MKJP0104 <m-kinoshita@jp-mic.local>
2019-03-19 14:47:16 +0900

    First Commit Paint Panel

---
+++ Command/CommandExample/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace CommandExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.paintPanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // paintPanel
+            // 
+            this.paintPanel.BackColor = System.Drawing.Color.White;
+            this.paintPanel.Location = new System.Drawing.Point(12, 12);
+            this.paintPanel.Name = "paintPanel";
+            this.paintPanel.Size = new System.Drawing.Size(282, 252);
+            this.paintPanel.TabIndex = 0;
+            this.paintPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.paintPanel_Paint);
+            this.paintPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.paintPanel_MouseDown);
+            this.paintPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.paintPanel_MouseMove);
+            this.paintPanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.paintPanel_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(306, 276);
+            this.Controls.Add(this.paintPanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel paintPanel;
+    }
+}
+
---
+++ Command/CommandExample/Form1.cs
@@ -0,0 +1,80 @@
+﻿using CommandExample.Draw;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CommandExample
+{
+    public partial class Form1 : Form
+    {
+        private Shapes _shapes = new Shapes();
+        private bool _isPainting = false;
+        private Point _lastPos = new Point();
+        private Color _currentColor = Color.Black;
+        private float _currentWidth = 10;
+        private int _shapeNumber = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //this.DoubleBuffered = true;
+            paintPanel.GetType().GetMethod("SetStyle",
+                System.Reflection.BindingFlags.Instance |
+                System.Reflection.BindingFlags.NonPublic).Invoke(paintPanel,
+                new object[] { ControlStyles.UserPaint | ControlStyles.AllPaintingInWmPaint | ControlStyles.OptimizedDoubleBuffer, true });
+        }
+
+        private void paintPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            _isPainting = true;
+            _shapeNumber++;
+            _lastPos = new Point(0, 0);
+        }
+
+        private void paintPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_isPainting)
+            {
+                if (_lastPos != e.Location)
+                {
+                    _lastPos = e.Location;
+                    _shapes.AddShape(_lastPos, _currentWidth, _currentColor, _shapeNumber);
+                }
+            }
+            paintPanel.Refresh();
+        }
+
+        private void paintPanel_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (_isPainting)
+            {
+                _isPainting = false;
+            }
+        }
+
+        private void paintPanel_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            for (int i = 0; i < _shapes.NumberOfShapes() - 1; i++)
+            {
+                var T = _shapes.GetShape(i);
+                var T1 = _shapes.GetShape(i + 1);
+                if (T.ShapeNumber == T1.ShapeNumber)
+                {
+                    Pen pen = new Pen(T.Color, T.Width);
+                    pen.StartCap = System.Drawing.Drawing2D.LineCap.Round;
+                    pen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+
+                    e.Graphics.DrawLine(pen, T.Location, T1.Location);
+                    pen.Dispose();
+                }
+            }
+        }
+    }
+}
