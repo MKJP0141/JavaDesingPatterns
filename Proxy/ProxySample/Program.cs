c38247416cc7599e8802eea43942fb4ba162aa56
MKJP0104 <m-kinoshita@jp-mic.local>
2019-04-01 14:57:00 +0900

    Add Proxy Example

---
+++ Proxy/ProxySample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ProxySample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IInternet internet = new ProxyInternet();
+            try
+            {
+                internet.ConnectTo("geeks.org");
+                internet.ConnectTo("abc.com");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"{e.StackTrace}");
+                //throw;
+            }
+        }
+    }
+}
---
+++ Proxy/ProxySample/ProxyInternet.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProxySample
+{
+    public class ProxyInternet : IInternet
+    {
+        private IInternet _internet = new RealInternet();
+        private static List<string> _bannedSites = new List<string>
+        {
+            "abc.com",
+            "def.com",
+            "ijk.com",
+            "lnm.com"
+        };
+
+        public virtual void ConnectTo(string serverHost)
+        {
+            if (_bannedSites.Contains(serverHost.ToLower()))
+            {
+                throw new Exception("Accesdd Denied.");
+            }
+            _internet.ConnectTo(serverHost);
+        }
+    }
+}
